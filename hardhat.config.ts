db384d5fe41bb5dd3db5a97fc27ba9e1c4a6f804
hyuktae-kwon <2007alex@naver.com>
2024-11-14 13:37:00 +0900

    style: remove keys

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -2,8 +2,8 @@ import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 import { vars } from "hardhat/config";
 
-const INFURA_API_KEY = vars.get("INFURA_API_KEY");
-const SEPOLIA_PRIVATE_KEY = vars.get("SEPOLIA_PRIVATE_KEY");
+// const INFURA_API_KEY = vars.get("INFURA_API_KEY");
+// const SEPOLIA_PRIVATE_KEY = vars.get("SEPOLIA_PRIVATE_KEY");
 
 const config: HardhatUserConfig = {
   solidity: "0.8.27",
@@ -15,10 +15,10 @@ const config: HardhatUserConfig = {
       blockGasLimit: 300000000,
       allowUnlimitedContractSize: true,
     },
-    sepolia: {
-      url: `https://sepolia.infura.io/v3/${INFURA_API_KEY}`,
-      accounts: [SEPOLIA_PRIVATE_KEY],
-    },
+    // sepolia: {
+    //   url: `https://sepolia.infura.io/v3/${INFURA_API_KEY}`,
+    //   accounts: [SEPOLIA_PRIVATE_KEY],
+    // },
   },
 };
 
